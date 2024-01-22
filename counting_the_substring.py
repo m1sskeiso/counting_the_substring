5b3fdaed96065729661f69cb9b8c06a3c63a8cef
Marck Christian Agulto <150341453+m1sskeiso@users.noreply.github.com>
2024-01-22 20:57:10 +0700

    final

--- counting_the_substring.py
+++ counting_the_substring.py
@@ -1,4 +1,6 @@
+# The sample sentence
 str_x = "Ang ganda ganda ni marck, nag iisang pinaka maganda sa mundo"
+
 #use the count method
 cnt = str_x.count("ganda")
 print("the word ganda is repeated", cnt, "times")
